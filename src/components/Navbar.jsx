66b4637feeab4ea472fab067b1b39e4029dcf342
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-02-24 18:10:32 +0600

    position avatar beside logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = () => {
     return (
         <header className="w-full h-14 sticky top-0 backdrop-blur-3xl">
             <div className='flex justify-between items-center h-full mx-auto w-5/6'>
-                <div>
-                    <h1 className='font-bold text-2xl'>Blog</h1>
+                <div className='flex justify-center items-center gap-4'>
+                	<div><UserAvatar /></div>
+                	<div><h1 className='font-bold text-2xl'>Blog</h1></div>
                 </div>
                 <div className="flex items-center gap-5">
                     <ul className='gap-5 hidden md:flex'>
                         <li><Link href={'/'}>Home</Link></li>
                         <li><Link href={'/blogs'}>Blogs</Link></li>
                     </ul>
-                    <UserAvatar /> {/* Use it with an uppercase name */}
                     <Hamburger className="flex md:hidden"/>
                 </div>
             </div>
