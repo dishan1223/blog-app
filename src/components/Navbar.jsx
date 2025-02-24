48d316c6b8566ed5bef6e5aa204c2b3ff10b721e
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-02-24 18:58:41 +0600

    remove avatar from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
-import UserAvatar from './avatar';
+
 import Hamburger from './Hamburger';
 
 const Navbar = () => {
     return (
         <header className="w-full h-14 sticky top-0 backdrop-blur-3xl">
             <div className='flex justify-between items-center h-full mx-auto w-5/6'>
-                <div className='flex justify-center items-center gap-4'>
-                	<div><UserAvatar /></div>
-                	<div><h1 className='font-bold text-2xl'>Blog</h1></div>
-                </div>
+                
+                <div><h1 className='font-bold text-2xl'>BlackBox</h1></div>
+                
                 <div className="flex items-center gap-5">
                     <ul className='gap-5 hidden md:flex'>
                         <li><Link href={'/'}>Home</Link></li>
