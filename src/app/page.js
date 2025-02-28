6bc8b9b7d0e5f62313d6a2ce0cb28f48ada080f1
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-02-28 18:13:29 +0600

    add some text to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { TextAnimate } from "@/components/magicui/text-animate";
 
 export default function Home() {
   return (
     <MaxWidthWrapper>
-    	<div className="w-full h-screen flex flex-col mt-8">
-    		lorem ipsum
+    	<div className="mt-10">
+    		<h1 className="font-bold text-4xl">BlackBox</h1>
+    		<p>
+    			<strong>Blackbox</strong>
+    			is a web development course designed to teach essential skills, including <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>, <strong>Git</strong>, <strong>React</strong>, and <strong>Next.js</strong> with <strong>Tailwind CSS</strong>. The course provides a structured learning experience, and regular class notes will be available for students to follow along easily.
+    		</p>
     	</div>
    	</MaxWidthWrapper>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import Hamburger from './Hamburger';
 
 const Navbar = () => {
     return (
-        <header className="w-full py-12 h-14 sticky top-0 border-b border-cyan-400 dark:border-orange-600 backdrop-blur-md">
+        <header className="w-full py-12 h-14 sticky top-0 border-b border-cyan-400 dark:border-orange-500/40 backdrop-blur-md">
             <div className='flex justify-between items-center h-full mx-auto w-[90%] md:w-[736px]'>
                 
                 <div>
-                    <Link href={' / '}><h1 className='font-bold font-ibm text-2xl test'>BlackBox</h1></Link>
+                    <Link href={' / '}><h1 className='font-bold text-2xl'>BlackBox</h1></Link>
                 </div>
                 
                 <div className="flex items-center gap-5">
