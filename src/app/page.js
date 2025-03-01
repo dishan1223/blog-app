aa19b00a79391bd44cf84020c257a90a1101f9ae
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-03-01 17:28:18 +0600

    fix font color bug on light mode

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <MaxWidthWrapper>
     	<div className="mt-10">
     		<h1 className="font-bold text-4xl mb-10">BlackBox</h1>
-    		<p className="mb-4 text-white font-normal">
+    		<p className="mb-4 dark:text-white font-normal">
     			<strong>Blackbox </strong>
     			is a web development course designed to teach essential skills, including <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>, <strong>Git</strong>, <strong>React</strong>, and <strong>Next.js</strong> with <strong>Tailwind CSS</strong>. The course provides a structured learning experience, and regular class notes will be available for students to follow along easily.
     		</p>
