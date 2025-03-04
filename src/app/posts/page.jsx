a2dc181e3f90c6bb625656df201e4c3dc51a886a
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-03-04 09:54:22 +0600

    add front matter retrieval
    in src/posts/page.jsx file, I implemented a map method to get all the posts inside of src/content dir.

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,15 +1,32 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
+import {getPosts} from "@/lib/posts"
 
 export default function Blogs(){
+    const posts = getPosts();
+
 	return (
 		<MaxWidthWrapper>
 			<div>
 				<p className="dark:text-[#AEB2BB] text-[#525152] ">
 				<Link href="/"><span className="dark:hover:text-white hover:text-slate-950">Home</span>
-				</Link> » Blogs
+				</Link> » Posts
 				</p>
-				
+			
+                <h1 className="font-bold text-3xl py-2">Posts</h1>
+                <p className="italic">All the articles that I have written.</p>
+
+                <div>
+                   {posts.map((post) => (
+                        <div>
+                            <Link href={`/posts/${post.title}`}>
+                                <h2>{post.title}</h2>
+                            </Link>
+                            <p>Updated on {post.date}</p>
+                            <p>{post.description}</p>
+                        </div>                       
+                   ))} 
+                </div>
 			</div>
 		</MaxWidthWrapper>
 	)
