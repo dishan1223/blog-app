7decca813f783f3eeb0450b861620edc3ac4003f
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-03-04 14:48:07 +0600

    add design for posts page
    some responsive design for post section in /posts

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,6 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import {getPosts} from "@/lib/posts"
+import Line from "@/components/Line";
 
 export default function Blogs(){
     const posts = getPosts();
@@ -15,15 +16,24 @@ export default function Blogs(){
 			
                 <h1 className="font-bold text-3xl py-2">Posts</h1>
                 <p className="italic">All the articles that I have written.</p>
-
-                <div>
+                <Line />
+                <div className='mt-4'>
                    {posts.map((post) => (
-                        <div>
-                            <Link href={`/posts/${post.title}`}>
-                                <h2>{post.title}</h2>
-                            </Link>
-                            <p>Updated on {post.date}</p>
-                            <p>{post.description}</p>
+                        <div className="flex flex-col gap-3 border-2 border-[#E2E8F0] dark:border-slate-600 rounded-md p-2">
+                            <div>
+                                <Link href={`/posts/${post.title}`}>
+                                    <h2 className="text-xl md:text-2xl">{post.title}</h2>
+                                </Link>
+                            </div>
+                            <div className="flex flex-col md:flex-row gap-2">
+                                {post.tags.map((tag) => (
+                                    <div className="px-3 py-1 bg-slate-200 rounded-3xl dark:bg-slate-700">{tag}</div>
+                                ))}
+                            </div>
+                            <div>
+                                <p className='text-gray-600 italic mb-3 dark:text-[#AEB2BB]'>Updated: {post.date}</p>
+                                <p>{post.description}</p>
+                            </div>
                         </div>                       
                    ))} 
                 </div>
