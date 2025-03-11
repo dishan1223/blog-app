bbb6d34f7e4d3f8a13a46202380c0ef4298713ae
dishan1223 <dishanishtiaq45+github@gmail.com>
2025-03-11 15:17:56 +0600

    modify site config

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,7 +1,7 @@
 const siteConfig = {
-  title: "WD Academy",
-  tagline: "Unlocking Knowledge, One Build at a Time",
-  description: "A journey into Next.js—learning by building real-world projects.",
+  title: "WebDev Academy",
+  tagline: "From beginner to pro in web development.",
+  description: "Learn web development with structured class notes, tutorials, and resources. Master HTML, CSS, JavaScript, React, Next.js, and more with our complete web development course.",
   author: "Ishtiaq Dishan",
   keywords: ["Next.js", "Web Development", "React", "Learning", "Projects"],
   url: "https://wdacademy.netlify.app",
